c96cdf651f32317939bf18cbe969151f9f079d24
a32fred <a.32.freeed@gmail.com>
2023-03-06 22:56:20 -0300

    initial header

---
+++ src/components/NavMobile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function NavMobile() {
+  return <div>NavMobile</div>;
+}
+
+export default NavMobile;
---
+++ src/components/Socials.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Socials() {
+  return <div>Socials</div>;
+}
+
+export default Socials;
